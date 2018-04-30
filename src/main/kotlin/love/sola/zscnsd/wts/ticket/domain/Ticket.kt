a3d14283d18d496f27a25e7659c02ca1b2950b5d
Sola <dev@sola.love>
2018-05-01 04:48:44 +0800

    propose Ticket domain model
    Signed-off-by: Sola <dev@sola.love>

--- src/main/kotlin/love/sola/zscnsd/wts/ticket/domain/Ticket.kt
+++ src/main/kotlin/love/sola/zscnsd/wts/ticket/domain/Ticket.kt
@@ -1,2 +1,22 @@
 package love.sola.zscnsd.wts.ticket.domain
 
+import love.sola.zscnsd.wts.common.domain.Address
+import java.time.LocalDateTime
+import javax.persistence.Embedded
+import javax.persistence.Entity
+import javax.persistence.Id
+import javax.persistence.Lob
+
+@Entity
+class Ticket(
+    @Id val id: Int,
+    val creator: Long,
+    val createTime: LocalDateTime,
+    val updateTime: LocalDateTime,
+    @Embedded val address: Address,
+    @Lob val description: String,
+    val phone: String,
+    val state: TicketState
+) {
+
+}
